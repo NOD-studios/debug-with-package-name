1f9a315e735176cc796d28cb1fc156efb5626e92
Furkan Tunalı <mail@furkantunali.com>
2018-09-01 08:54:15 +0200

    rename

--- src/lib.js
+++ src/lib.js
@@ -1,7 +1,7 @@
 // @flow
 import debug from 'debug'
 import { sync as readPkgSync } from 'read-pkg-up'
-import { dirname, join } from 'path'
+import { join } from 'path'
 import { name as packageName } from '../package.json'
 
 export const debugInternal = (
@@ -9,15 +9,15 @@ export const debugInternal = (
   description: ?string = ' %O'
 ): any => [debug(packageName)(description, variable), variable][1]
 
+export const getPackageName = (dir?: string = process.cwd()): string => {
+  const pkg = readPkgSync({ cwd: dir })
+  const {
+    pkg: { name }
+  } = pkg
+  return name
+}
+
 export const defaultTemplate = (
   extraPrefix?: string = '',
   packagePrefix?: string = getPackageName()
 ): string => debugInternal(join(packagePrefix, extraPrefix), 'defaultTemplate')
-
-export const getPackageName = (dir?: string = process.cwd()): string => {
-  const pkg = readPkgSync({ cwd: dir })
-  const {
-    pkg: { name },
-  } = pkg
-  return name
-}
